6e4a80bee16695323e2b290b17160705277b9213
Imou-OpenPlatform <cloud_openteam_service@imou.com>
2026-07-07 20:32:37 +0800

    chore: release pyimouapi 1.3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pyimouapi",
-    version="1.2.9",
+    version="1.3.0",
     packages=find_packages(),
     python_requires=">=3.11",
     install_requires=[
